d8daa5bcfe62ee0920f5489dd0e212c62568abe8
unknown <fabellaivan@hotmail.com>
2022-04-11 20:55:16 -0300

    Programas en estado Final

---
+++ Prueba/src/Prueba.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : Prueba.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int numero=1;
+	printf("Ingrese variable");
+	scanf("%d",&numero);
+	printf("%d",numero);
+	return EXIT_SUCCESS;
+}
